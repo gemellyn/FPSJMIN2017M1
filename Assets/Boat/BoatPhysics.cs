4c603199f187555a0dca52761eb515e6e192f510
guillaume <guillaume.levieux@cnam.fr>
2017-10-27 15:39:49 +0200

    Spawn

---
+++ Assets/Boat/BoatPhysics.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatPhysics : MonoBehaviour {
+
+    public Transform Water;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        float profondeur = transform.position.y - Water.position.y;
+        //Debug.Log(profondeur);
+        Vector3 forceUp = Vector3.up * Mathf.Pow(Mathf.Abs(profondeur), 3.0f) * 10;
+        if (profondeur < 0)
+        {
+            GetComponent<Rigidbody>().AddForce(forceUp);
+        }
+        else
+            if(profondeur < 1.0f)
+                GetComponent<Rigidbody>().AddForce(-forceUp);
+    }
+}
---
+++ Assets/Spawner/SpaxnBirdie.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaxnBirdie : MonoBehaviour {
+
+    public Transform Birdie;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.name == "Player")
+        {
+            for(int i = -10; i < 10; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    Birdie.gameObject.SetActive(true);
+                    Vector3 position = Camera.main.transform.position;
+                    position += -Vector3.right * 100;
+                    position += -Vector3.right * j * 20;
+                    position += Vector3.forward * i * 20;
+                    Instantiate(Birdie, position, Quaternion.identity);
+                }
+            }
+            
+        }
+    }
+
+
+}
